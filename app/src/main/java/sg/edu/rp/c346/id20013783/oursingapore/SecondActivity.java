7e1128548b5cba28f0c552030c5bb070d455540a
20013783@rp.edu.sg <3@Dogs1kenzp>
2021-08-10 22:48:59 +0800

    Our singapore- enhanced

--- app/src/main/java/sg/edu/rp/c346/id20013783/oursingapore/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20013783/oursingapore/SecondActivity.java
@@ -1,15 +1,22 @@
 package sg.edu.rp.c346.id20013783.oursingapore;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -20,6 +27,7 @@ public class SecondActivity extends AppCompatActivity {
     CustomAdapter adapter;
     int requestCode = 9;
     Button btn5Stars;
+    Button btnInsert;
 
 
     @Override
@@ -30,6 +38,7 @@ public class SecondActivity extends AppCompatActivity {
 
         lv = (ListView) this.findViewById(R.id.lv);
         btn5Stars = (Button) this.findViewById(R.id.btnShow5Stars);
+        btnInsert = (Button) this.findViewById(R.id.btninsert);
 
         DBHelper dbh = new DBHelper(this);
         islandList = dbh.getAllIsland();
@@ -54,6 +63,62 @@ public class SecondActivity extends AppCompatActivity {
                 islandList.clear();
                 islandList.addAll(dbh.getAllIslandByStar(5));
                 adapter.notifyDataSetChanged();
+            }
+        });
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Inflate the input.xml layout file
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View viewDialog = inflater.inflate(R.layout.input,null);
+
+                final EditText etInputTitle = viewDialog.findViewById(R.id.etInputTitle);
+                final EditText etInputDescription = viewDialog.findViewById(R.id.etInputDescription);
+                final EditText etInputArea = viewDialog.findViewById(R.id.etInputArea);
+                final RatingBar etRating = viewDialog.findViewById(R.id.inputratingBar);
+
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(SecondActivity.this);
+                myBuilder.setView(viewDialog);
+                myBuilder.setTitle("Insert Island");
+                myBuilder.setPositiveButton("INSERT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String title = etInputTitle.getText().toString().trim();
+                        String description = etInputDescription.getText().toString().trim();
+                        if (title.length() == 0 || description.length() == 0){
+                            Toast.makeText(SecondActivity.this, "Incomplete data", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        String area_str = etInputArea.getText().toString().trim();
+                        int area = 0;
+                        try {
+                            area = Integer.valueOf(area_str);
+                        } catch (Exception e) {
+                            Toast.makeText(SecondActivity.this, "Invalid island", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        DBHelper dbh = new DBHelper(SecondActivity.this);
+                        int rating = (int)etRating.getRating();
+                        long result = dbh.insertIsland(title, description, area, rating);
+                        adapter.notifyDataSetChanged();
+                        dbh.close();
+                        if (result != -1) {
+                            Toast.makeText(SecondActivity.this, "Island inserted", Toast.LENGTH_LONG).show();
+                            etInputTitle.setText("");
+                            etInputDescription.setText("");
+                            etInputArea.setText("");
+                        } else {
+                            Toast.makeText(SecondActivity.this, "Insert failed", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+                myBuilder.setNegativeButton("CANCEL",null);
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
             }
         });
     }
